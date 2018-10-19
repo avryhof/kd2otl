916b1ee3df37c25ccde6e6135bb0a2fdeefb926f
avryhof <amos@vryhofresearch.com>
2018-10-19 11:28:14 -0400

    I only really know English

--- kd2otl/settings.py
+++ kd2otl/settings.py
@@ -168,6 +168,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
+LANGUAGES = [
+    ('en-us', 'English'),
+]
 
 LANGUAGE_CODE = 'en'
 
